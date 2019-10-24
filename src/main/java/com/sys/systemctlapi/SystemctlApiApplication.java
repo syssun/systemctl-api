3609c7d29dfd035b8405ab5d76e3b6c26cb85369
syssun <2506715686@qq.com>
2019-10-24 17:30:29 +0800

    123

--- src/main/java/com/sys/systemctlapi/SystemctlApiApplication.java
+++ src/main/java/com/sys/systemctlapi/SystemctlApiApplication.java
@@ -1,13 +1,48 @@
 package com.sys.systemctlapi;
 
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SystemctlApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SystemctlApiApplication.class, args);
+    }
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        String os = System.getProperty("os.name");
+        if(os.toLowerCase().contains("win")){   //在本地window环境测试时用localhost
+            System.out.println("this is  windows");
+            config.setHostname("localhost");
+        } else {
+            config.setHostname("106.54.123.133");   //部署到你的远程服务器正式发布环境时用服务器公网ip
+        }
+        config.setPort(9088);
+       // config.setTransports(Transport.POLLING, Transport.WEBSOCKET);
+        config.setOrigin(":*:");
+        /*config.setAuthorizationListener(new AuthorizationListener() {//类似过滤器
+            @Override
+            public boolean isAuthorized(HandshakeData data) {
+                //http://localhost:8081?username=test&password=test
+                //例如果使用上面的链接进行connect，可以使用如下代码获取用户密码信息，本文不做身份验证
+                // String username = data.getSingleUrlParam("username");
+                // String password = data.getSingleUrlParam("password");
+                return true;
+            }
+        });*/
+        final SocketIOServer server = new SocketIOServer(config);
+        return server;
+    }
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketServer) {
+        return new SpringAnnotationScanner(socketServer);
     }
 
 }
--- src/main/java/com/sys/systemctlapi/kafka/consumer/ConsumerListener.java
+++ src/main/java/com/sys/systemctlapi/kafka/consumer/ConsumerListener.java
@@ -1,8 +1,11 @@
 package com.sys.systemctlapi.kafka.consumer;
+import com.sys.systemctlapi.socket.MessageEventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * 消费者监听 topic
@@ -10,11 +13,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConsumerListener {
     @Autowired
-    private KafkaTemplate<String,Object> kafkaTemplate;
+    private MessageEventHandler messageEventHandler; //socketio
     //接受到指令
     @KafkaListener(topics = "lockscreenTopicRes")
     public void lockscreenTopicRes(String message){
         System.out.println("接收到结果指令");
         System.out.println(message);  //通过socket 发送到终端
+        for (UUID clientId : messageEventHandler.listClient) {
+            if (messageEventHandler.socketIoServer.getClient(clientId) == null)
+                continue;
+            //发送消息
+            messageEventHandler.socketIoServer.getClient(clientId).sendEvent("messageevent", message);
+        }
     }
 }
